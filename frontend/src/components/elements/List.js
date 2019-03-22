fae96c864b01a808453fc98a52336f7972a0d294
Dan Collins <dcollins31@gmail.com>
2019-03-21 23:29:53 -0500

    Added table in catalog

--- frontend/src/components/elements/List.js
+++ frontend/src/components/elements/List.js
@@ -1,47 +1,52 @@
 import React, { Component } from 'react';
 import './List.css';
 
-class Accordion extends Component {
+class List extends Component {
     render() {
       return (
         <div className="container">
-            <table className="table table-hover">
-          <thead>
-            <tr>
-              <th className="bg-dark head-txt" scope="col">CABLES</th>
-              <th className="bg-dark head-txt" scope="col">PART #</th>
-             </tr>
-          </thead>
-          <tbody>
-            <tr>
-            <td><strong>AUDIO</strong></td>
-            </tr>
-            <tr>
-            <td>SINGLE PAIR</td>
-
-            </tr>
-             <tr>
-              <th scope="row">Analog Single-Pair Shielded: 22 AWG</th>
-            </tr>
-            <tr>
-              <th scope="row">Analog Single-Pair Shielded: 22 AWG
-              <i className="fas fa-fire fa-lg" /> Plenum
-              </th>
-            </tr>
-            <tr>
-              <th scope="row">AES/EBU Digital Single-Pair: 24 AWG
-              <i class="fas fa-hdd fa-lg" /> Digital
-              </th>
-            </tr>
-            <tr>
-              <th scope="row">AES/EBU Digital Single-Pair: 26 AWG
-              <i class="fas fa-hdd fa-lg" /> Digital
-              </th>
-            </tr>
-            <td><strong>DUAL PAIR</strong></td>
-            <tr>
-              <th scope="row">Analog Dual-Pair Shielded: 22 AWG</th>
-            </tr>
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th className="bg-dark head-tbl" scope="col">CABLES</th>
+                <th className="bg-dark head-tbl" scope="col">PART #</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="audio"><strong>AUDIO</strong></td>
+                <td className="audio"></td>
+              </tr>
+              <tr>
+                <th scope="row">SINGLE PAIR</th>
+                <td></td>
+              </tr>
+              <tr>
+                <th scope="row">Analog Single-Pair Shielded: 22 AWG</th>
+                <td>SPA22GS</td>
+              </tr>
+              <tr>
+                <th scope="row">Analog Single-Pair Shielded: 22 AWG Plenum
+                <i className="fas fa-fire fa-lg" /> 
+                </th>
+                <td>SPA22GS-PLEN</td>
+              </tr>
+              <tr>
+                <th scope="row">AES/EBU Digital Single-Pair: 24 AWG
+                <i class="fas fa-hdd fa-lg" />
+                </th>
+                <td>901</td>
+              </tr>
+              <tr>
+                <th scope="row">AES/EBU Digital Single-Pair: 26 AWG
+                <i class="fas fa-hdd fa-lg" />
+                </th>
+                <td>801</td>
+              </tr>
+              <tr>
+                <th scope="row">DUAL PAIR</th>
+                <td></td>
+              </tr>
             </tbody>
           </table>
 
@@ -51,4 +56,4 @@ class Accordion extends Component {
       );
     }
   }
-export default Accordion;
+export default List;
